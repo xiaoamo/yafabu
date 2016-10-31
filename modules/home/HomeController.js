a0d9474a27a67dcc27e57d086c1519f6bbe621ec
yinkun <276862013@qq.com>
2016-10-31 11:07:39 +0800

    '在分支上完成首页页面并且完成路由的优化'

--- modules/home/HomeController.js
+++ modules/home/HomeController.js
@@ -1,11 +1,42 @@
 define(['router','css!modules/home/home.css','swiper'], function (app,css,swiper) {
-	 //angular会自动根据controller函数的参数名，导入相应的服务
-	  return app.controller('HomeController',['$scope','$http', '$timeout','scroll',function ($scope,$http,$timeout,scroll) {
-	  	$scope.info = '首页内容';
-//	  	$http.get('data/home.json').success(function(res){
-//      	$scope.data = res;
-//		});
-	  }]);
-	  
-	  
+	//自定义一个服务;
+	app.factory('scroll',function () {
+		return {
+			scrollToBottom:function () {
+				$(window).on('scroll',function () {
+					if(document.body.scrollTop > 40){
+						$('.home_top').css('background','url(img/homebg.jpg)');
+					}else{
+						$('.home_top').css('background',' rgba(0, 0, 200, 0)');
+					}
+				})
+			}
+		}
+	});
+    return app.controller('HomeController',['$scope','$http', '$timeout','scroll',function ($scope,$http,$timeout,scroll) {
+        $scope.info = '首页内容';
+        $http.get('data/home.json').success(function(res){
+        	$scope.datanav = res.data.bar;
+        	$scope.datapromo=res.data.promo;
+        	$scope.dataGars=res.data.gears;
+        	$scope.hotTopic=res.data.hot_topic;
+//      	$scope.elite=res.data.hot_topic.elite;
+        	console.log($scope.elite);
+		});
+		 $http.get('data/like_1.json').success(function(res){
+		 	$scope.love=res.data
+		 	console.log($scope.love);
+		 });
+        //使用swiper插件需要使用angular的定时器
+		$timeout(function(){
+			$scope.swiper =  new Swiper(".swiper-container",{
+        		loop : true,
+        		pagination: '.swiper-pagination',
+        		autoplay:2000,
+        		autoplayDisableOnInteraction:false
+        	});
+		},0)
+		scroll.scrollToBottom();
+	}])
 });
+
